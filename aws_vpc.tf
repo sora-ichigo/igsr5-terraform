0fc571885f6a6ba8a3b85f4925ec699eea54ec8b
ichigo-dev <ichigo@web-freework.com>
2021-11-24 22:57:01 +0900

    Comment out nat gateway

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -92,18 +92,19 @@ resource "aws_route_table" "private_1" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route" "private_0" {
-  route_table_id = aws_route_table.private_0.id
-  nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
-resource "aws_route" "private_1" {
-  route_table_id = aws_route_table.private_1.id
-  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
+# resource "aws_route" "private_0" {
+#   route_table_id = aws_route_table.private_0.id
+#   nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
+#   destination_cidr_block = "0.0.0.0/0"
+# }
+# 
+# resource "aws_route" "private_1" {
+#   route_table_id = aws_route_table.private_1.id
+#   nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+#   egress_only_gateway_id = aws_internet_gateway.example.id
+#   destination_cidr_block = "0.0.0.0/0"
+# }
+# 
 resource "aws_route_table_association" "private_0" {
   subnet_id = aws_subnet.private_0.id
   route_table_id = aws_route_table.private_0.id
@@ -117,24 +118,24 @@ resource "aws_route_table_association" "private_1" {
 /*
  * nat gateway
  * */
-resource "aws_eip" "nat_gateway_0" {
-  vpc = true
-  depends_on = [aws_internet_gateway.example]
-}
+# resource "aws_eip" "nat_gateway_0" {
+#   vpc = true
+#   depends_on = [aws_internet_gateway.example]
+# }
 
-resource "aws_nat_gateway" "nat_gateway_0" {
-  allocation_id = aws_eip.nat_gateway_0.id
-  subnet_id = aws_subnet.public_0.id
-  depends_on = [aws_internet_gateway.example]
-}
+# resource "aws_nat_gateway" "nat_gateway_0" {
+#   allocation_id = aws_eip.nat_gateway_0.id
+#   subnet_id = aws_subnet.public_0.id
+#   depends_on = [aws_internet_gateway.example]
+# }
 
-resource "aws_eip" "nat_gateway_1" {
-  vpc = true
-  depends_on = [aws_internet_gateway.example]
-}
+# resource "aws_eip" "nat_gateway_1" {
+#   vpc = true
+#   depends_on = [aws_internet_gateway.example]
+# }
 
-resource "aws_nat_gateway" "nat_gateway_1" {
-  allocation_id = aws_eip.nat_gateway_1.id
-  subnet_id = aws_subnet.public_1.id
-  depends_on = [aws_internet_gateway.example]
-}
+# resource "aws_nat_gateway" "nat_gateway_1" {
+#   allocation_id = aws_eip.nat_gateway_1.id
+#   subnet_id = aws_subnet.public_1.id
+#   depends_on = [aws_internet_gateway.example]
+# }
