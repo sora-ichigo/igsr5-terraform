be2d660a8c1de8a19d36b87344ed6e8eae141844
ichigo-dev <ichigo@web-freework.com>
2021-12-31 17:12:15 +0900

    Update

--- aws/vpc_endpoint_route_table_association.tf
+++ aws/vpc_endpoint_route_table_association.tf
@@ -2,7 +2,3 @@
 //   vpc_endpoint_id = aws_vpc_endpoint.s3.id
 //   route_table_id  = aws_route_table.igsr5_private.id
 // }
-resource "aws_vpc_endpoint_route_table_association" "private_s3_0" {
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-  route_table_id  = aws_route_table.igsr5_public.id
-}
