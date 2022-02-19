a56cffc35c0786249b114ae31d7e79a5fa211117
igsr5 <i@gsr5.dev>
2022-02-19 22:21:01 +0900

    Add images.igsr5.com

--- aws/route53.tf
+++ aws/route53.tf
@@ -26,6 +26,21 @@ resource "aws_route53_record" "igsr5_sandbox_muson" {
   }
 }
 
+resource "aws_route53_record" "igsr5_images" {
+  zone_id = data.aws_route53_zone.igsr5.zone_id
+  name    = format("images.%s", data.aws_route53_zone.igsr5.name)
+  type    = "A"
+
+  alias {
+    name    = aws_cloudfront_distribution.igsr5-images.domain_name
+    zone_id = aws_cloudfront_distribution.igsr5-images.hosted_zone_id
+
+    evaluate_target_health = true
+  }
+}
+
+
+
 resource "aws_route53_record" "igsr5_certificate" {
   name    = tolist(aws_acm_certificate.igsr5.domain_validation_options)[0].resource_record_name
   type    = tolist(aws_acm_certificate.igsr5.domain_validation_options)[0].resource_record_type
