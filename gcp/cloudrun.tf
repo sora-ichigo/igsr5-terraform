ec80d36619cef1d57b2bef626d684f753aefc9c1
igsr5 <igsr5.dev@gmail.com>
2022-11-13 18:45:25 +0900

    Fix cloudrun resource name

--- gcp/cloudrun.tf
+++ gcp/cloudrun.tf
@@ -2,7 +2,7 @@
  * 友人に毎週月曜日に講義の出席を依頼するボット
  */
 resource "google_cloud_run_service" "attendance_request_bot" {
-  name     = "attendance_request_bot"
+  name     = "attendance-request-bot"
   location = "asia-northeast1"
 
   template {
