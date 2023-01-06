0dc079678b5bead8d909b6e8bde85f523f173985
igsr5 <igsr5.dev@gmail.com>
2023-01-07 02:18:10 +0900

    Add resource for igsr5_daily

--- gcp/cloudrun.tf
+++ gcp/cloudrun.tf
@@ -1,8 +1,31 @@
 /*
+ * attendance_request_bot
  * 友人に毎週月曜日に講義の出席を依頼するボット
  */
 resource "google_cloud_run_service" "attendance_request_bot" {
   name     = "attendance-request-bot"
+  location = "asia-northeast1"
+
+  template {
+    spec {
+      containers {
+        // NOTE: This is temporary. This will be replaced with a container image.
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [template]
+  }
+}
+
+/*
+ * igsr5_daily
+ * igsr5の日報アプリ
+ */
+resource "google_cloud_run_service" "igsr5_daily" {
+  name     = "igsr5_daily"
   location = "asia-northeast1"
 
   template {
--- gcp/cloudscheduler.tf
+++ gcp/cloudscheduler.tf
@@ -1,3 +1,7 @@
+/*
+ * attendance_request_bot
+ * 友人に毎週月曜日に講義の出席を依頼するボット
+ */
 resource "google_cloud_scheduler_job" "attendance_request_bot" {
   name             = "attendance-request-bot"
   description      = "Sends a message to the attendance-request-bot channel"
@@ -12,6 +16,33 @@ resource "google_cloud_scheduler_job" "attendance_request_bot" {
   http_target {
     http_method = "POST"
     uri         = google_cloud_run_service.attendance_request_bot.status[0].url
+
+    oidc_token {
+      audience              = google_cloud_run_service.attendance_request_bot.status[0].url
+      service_account_email = google_service_account.attendance_request_scheduler_sa.email
+    }
+  }
+}
+
+/*
+ * igsr5_daily
+ * igsr5の日報アプリ
+ *
+ * 毎日18時にSlackで日報作成を促す
+ */
+resource "google_cloud_scheduler_job" "igsr5_daily" {
+  name             = "igsr5_daily"
+  schedule         = "0 18 * * *"
+  time_zone        = "Asia/Tokyo"
+  attempt_deadline = "600s"
+
+  retry_config {
+    retry_count = 3
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = format("%s %s", google_cloud_run_service.igsr5_daily.status[0].url, "/prompt_daily_report")
 
     oidc_token {
       audience              = google_cloud_run_service.attendance_request_bot.status[0].url
