ce85cac92f1fd85336761b7c3da3d91ee0b9b7c8
igsr5 <igsr5.dev@gmail.com>
2023-01-15 22:55:01 +0900

    Decrease retry_count of igsr5-daily

--- gcp/cloudscheduler.tf
+++ gcp/cloudscheduler.tf
@@ -9,15 +9,15 @@
 //   schedule         = "45 10 * * 1"
 //   time_zone        = "Asia/Tokyo"
 //   attempt_deadline = "600s"
-// 
+//
 //   retry_config {
 //     retry_count = 3
 //   }
-// 
+//
 //   http_target {
 //     http_method = "POST"
 //     uri         = google_cloud_run_service.attendance_request_bot.status[0].url
-// 
+//
 //     oidc_token {
 //       audience              = google_cloud_run_service.attendance_request_bot.status[0].url
 //       service_account_email = google_service_account.attendance_request_scheduler_sa.email
@@ -38,7 +38,7 @@ resource "google_cloud_scheduler_job" "igsr5_daily" {
   attempt_deadline = "600s"
 
   retry_config {
-    retry_count = 3
+    retry_count = 1
   }
 
   http_target {
