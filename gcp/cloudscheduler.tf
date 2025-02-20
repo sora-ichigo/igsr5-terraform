a3b7d94e8c2fe6c1c7dca619e9587b1ef6155cab
Sora ichigo <igsr5.dev@gmail.com>
2025-02-20 11:02:28 +0900

    Remove unnecessary cloudscheduler

--- gcp/cloudscheduler.tf
+++ gcp/cloudscheduler.tf
@@ -2,28 +2,6 @@
  * attendance_request_bot
  * 友人に毎週月曜日に講義の出席を依頼するボット
  */
-// NOTE: なぜかterrform管理から抜けていたので手動管理に移行した
-// resource "google_cloud_scheduler_job" "attendance_request_bot" {
-//   name             = "attendance-request-bot"
-//   description      = "Sends a message to the attendance-request-bot channel"
-//   schedule         = "45 10 * * 1"
-//   time_zone        = "Asia/Tokyo"
-//   attempt_deadline = "600s"
-//
-//   retry_config {
-//     retry_count = 3
-//   }
-//
-//   http_target {
-//     http_method = "POST"
-//     uri         = google_cloud_run_service.attendance_request_bot.status[0].url
-//
-//     oidc_token {
-//       audience              = google_cloud_run_service.attendance_request_bot.status[0].url
-//       service_account_email = google_service_account.attendance_request_scheduler_sa.email
-//     }
-//   }
-// }
 
 /*
  * igsr5_daily
